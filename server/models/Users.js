7655ca206d5f7e793ac19435c6f77053b53997a9
sourabh <201210048@nitdelhi.ac.in>
2023-08-19 15:55:16 +0530

    added models folder to work on

---
+++ server/models/Users.js
@@ -0,0 +1,6 @@
+// create a user model
+// const User = mongoose.model('User', { // create a model called User
+//     username: String, // username is a string
+//     password: String // password is a string
+// });
+//
