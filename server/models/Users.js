38dcd404242d04e696e7539be346a916e57c63f5
sourabh <201210048@nitdelhi.ac.in>
2023-08-19 16:00:41 +0530

    added user schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,6 +1,24 @@
-// create a user model
-// const User = mongoose.model('User', { // create a model called User
-//     username: String, // username is a string
-//     password: String // password is a string
-// });
-//
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({ // create a new mongoose schema
+    username: {
+        type: String, // type is string
+        required: true, // required is true
+        unique: true, // unique is true
+        trim: true, // trim is true
+        minlength: 3 // minlength is 3
+    },
+    password: {
+        type: String, // type is string
+        required: true, // required is true
+        trim: true, // trim is true
+        minlength: 3 // minlength is 3
+    },
+}, {
+    timestamps: true, // timestamps is true
+});
+
+const User = mongoose.model('Users', userSchema); // create a new mongoose model
+
+export default User; // export User
+
