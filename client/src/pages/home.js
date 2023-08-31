05dfabad3ae923a11a5a15d017f6fe74df53f42b
Saurav <choudharysaurav724@gmail.com>
2023-08-31 22:44:52 +0530

    done with home page to display objects in database

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,76 @@
+import React, { useEffect, useState } from "react";
+// import { useGetUserID } from "../hooks/useGetUserId";
+import axios from "axios";
+
+
 export const Home = () => {
-  return(
+  const [recipes, setRecipes] = useState([]);
+  // const [savedRecipes, setSavedRecipes] = useState([]);
+
+  // const userID = useGetUserID();
+  // const userID=window.localStorage.getItem("userId");
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data.recipes);
+        console.log(response.data.recipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    // const fetchSavedRecipes = async () => {
+    //   try {
+    //     const response = await axios.get(
+    //       `http://localhost:3001/recipes/savedRecipes/ids/${userID}`
+    //     );
+    //     setSavedRecipes(response.data.savedRecipes);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+
+    fetchRecipes();
+    // fetchSavedRecipes();
+  }, []);
+
+  // const saveRecipe = async (recipeID) => {
+  //   try {
+  //     const response = await axios.put("http://localhost:3001/recipes", {
+  //       recipeID,
+  //       userID,
+  //     });
+  //     setSavedRecipes(response.data.savedRecipes);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+
+
+  return (
     <div>
-      <h1>Home</h1>
-      <p>This is the home page.</p>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.title}</h2>  
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.title} />
+            <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+
+
+};
